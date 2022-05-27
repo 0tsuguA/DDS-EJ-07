82f5606175e0ca3c87e41d14885e1bcbbe9a20b4
0tsuguA <augusstotl@gmail.com>
2022-05-27 20:22:57 -0300

    While

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Mostrar la tabla de multiplicar del 3, utilizando un while (Hasta el numero 20).
+//Se debe mostrar al similar a lo siguiente.
+//3 * 1 = 3
+//3 * 2 = 6
+//3 * 3 = 9
+
+$i=1;
+
+while ($i <= 20) {
+    $resultado=3*$i; 
+    echo ' 3*'. $i. '= '. $resultado. '<hr>' ;
+    echo $i++;
+}
